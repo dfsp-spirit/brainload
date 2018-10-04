f61ba3823d95225c3349758157f574b305702c50
Tim Schaefer <ts+code@rcmd.org>
2018-10-04 17:18:22 +0200

    add short explanation to demo client

--- example_client/run_example_no_install.sh
+++ example_client/run_example_no_install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+## This shell script runs a demo client which uses cogload without installation, directly from the source directory.
+## It is for internal development only and will not work on other machines as it loads data that only exists on my machine. I guess you could supply your own MRI/FreeSurfer data and use it, but it does nothing to fancy.
 STUDY_DATA_DIR="${HOME}/data/tim_only"
 COGLOAD_DIR="${HOME}/develop/cogload"
 COGLOAD_SOURCE_DIR="${COGLOAD_DIR}/src"
