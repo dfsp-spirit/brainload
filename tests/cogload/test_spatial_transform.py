8f24f2b012b6c9bf9f9fae69d415105386278719
Tim Schaefer <ts+code@rcmd.org>
2018-10-04 14:32:08 +0200

    test deg2rad with negative input value

--- tests/cogload/test_spatial_transform.py
+++ tests/cogload/test_spatial_transform.py
@@ -2,10 +2,14 @@ import pytest
 import numpy as np
 import cogload.spatial_transform as st
 
-def test_rad2deg_with_valid_input():
+def test_rad2deg_with_positive_input():
     deg = st.rad2deg(2 * np.pi)
     assert deg == pytest.approx(360.0, 0.01)
 
-def test_deg2rad_with_valid_input():
+def test_deg2rad_with_positive_input():
     rad = st.deg2rad(180)
     assert rad == pytest.approx(np.pi, 0.01)
+
+def test_deg2rad_with_negative_input():
+    rad = st.deg2rad(-90)
+    assert rad == pytest.approx(np.pi * 1.5, 0.01)
