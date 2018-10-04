cec8c8d41374c973acfd73b6839b60e8481d7a2d
Tim Schaefer <ts+code@rcmd.org>
2018-10-04 14:16:26 +0200

    add test for deg2rad

--- tests/cogload/test_spatial_transform.py
+++ tests/cogload/test_spatial_transform.py
@@ -5,3 +5,7 @@ import cogload.spatial_transform as st
 def test_rad2deg_with_valid_input():
     deg = st.rad2deg(2 * np.pi)
     assert deg == pytest.approx(360.0, 0.01)
+
+def test_deg2rad_with_valid_input():
+    rad = st.deg2rad(180)
+    assert rad == pytest.approx(np.pi, 0.01)
