bffdf47db162fb2bdf99257244db568d3da5f31c
Tim Schaefer <ts+code@rcmd.org>
2018-10-05 12:34:40 +0200

    add categories to setup file, remove useless exclusion of tests dir from findpackages call

--- setup.py
+++ setup.py
@@ -12,12 +12,17 @@ setup(
     keywords='neuroimaging freesurfer nibabel load mgh curv',
     author='Tim Schäfer',
     url='https://github.com/dfsp-spirit/cogload',
-    packages=find_packages(where='src', exclude='tests'),
+    packages=find_packages(where='src'),
+    classifiers = ['Development Status :: 2 - Pre-Alpha',                               # see https://pypi.org/pypi?%3Aaction=list_classifiers for full classifier list
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2'],
     license='MIT',
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     install_requires=['numpy', 'nibabel'],
-    extras_require={'plot_brain': ['mayavi']},
+    extras_require={'plot_brain': ['mayavi']},     # to use this, you have to `pip install cogload[plot_brain]`
     package_dir = {'': 'src'},             # The root directory that contains the source for the modules (relative to setup.py) is ./src/
     zip_safe=False
 )
