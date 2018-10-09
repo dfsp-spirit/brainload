e205c717648d115a927c897f1442f422be1f12f1
Tim Schaefer <ts+code@rcmd.org>
2018-10-09 10:52:35 +0200

    add more unit tests for spatial transform and fix bugs discovered while doing so

--- src/cogload/spatial_transform.py
+++ src/cogload/spatial_transform.py
@@ -2,31 +2,34 @@
 
 import numpy as np
 
-def rotate_3D_coordinates_around_axis(x, y, z, rot, axis):
-    '''Rotate coordinates around the given axis. Rotation must be given in radians.'''
-    if axis not in ('x', 'y', 'z'):
-        raise ValueError("ERROR: axis must be one of {'x', 'y', 'z'}")
-
-    if axis == 'x':
-        return rotate_3D_coordinates_around_x_axis(x, y, z, rot)
-    elif axis == 'y':
-        return rotate_3D_coordinates_around_y_axis(x, y, z, rot)
-    else:
-        return rotate_3D_coordinates_around_y_axis(x, y, z, rot)
-
 def rotate_3D_coordinates_around_axes(x, y, z, rotx, roty, rotz):
     '''Rotate coordinates around the axes. Rotation must be given in radians.'''
-    xr, yr, zr = rotate_3D_coordinates_around_x_axis(x, y, z, rotx)
-    xr, yr, zr = rotate_3D_coordinates_around_y_axis(xr, yr, zr, roty)
-    xr, yr, zr = rotate_3D_coordinates_around_z_axis(xr, yr, zr, rotz)
+    xr, yr, zr = _rotate_3D_coordinates_around_x_axis(x, y, z, rotx)
+    xr, yr, zr = _rotate_3D_coordinates_around_y_axis(xr, yr, zr, roty)
+    xr, yr, zr = _rotate_3D_coordinates_around_z_axis(xr, yr, zr, rotz)
     return xr, yr, zr
 
 
-def rotate_3D_coordinates_around_x_axis(x, y, z, rot):
+def _rotate_3D_coordinates_around_x_axis(x, y, z, rot):
     y_rotated  = np.cos(rot) * y - np.sin(rot) * z
     z_rotated  = np.sin(rot) * y + np.cos(rot) * z
     x_rotated  = x
     return x_rotated, y_rotated, z_rotated
+
+
+def _rotate_3D_coordinates_around_y_axis(x, y, z, rot):
+    z_rotated = np.cos(rot) * z - np.sin(rot) * x
+    x_rotated = np.sin(rot) * z + np.cos(rot) * x
+    y_rotated = y
+    return x_rotated, y_rotated, z_rotated
+
+
+def _rotate_3D_coordinates_around_z_axis(x, y, z, rot):
+    x_rotated = np.cos(rot) * x - np.sin(rot) * y
+    y_rotated = np.sin(rot) * x + np.cos(rot) * y
+    z_rotated = z
+    return x_rotated, y_rotated, z_rotated
+
 
 def coords_a2s(coords):
     """
@@ -35,7 +38,7 @@ def coords_a2s(coords):
     x = coords[:,0]
     y = coords[:,1]
     z = coords[:,2]
-    return x, y, z
+    return np.asarray(x), np.asarray(y), np.asarray(z)
 
 def coords_s2a(x, y, z):
     """
@@ -48,18 +51,13 @@ def coords_s2a(x, y, z):
     return np.column_stack((x, y, z))
 
 
-def rotate_3D_coordinates_around_y_axis(x, y, z, rot):
-    z_rotated = np.cos(rot) * z - np.sin(rot) * x
-    x_rotated = np.sin(rot) * z + np.cos(rot) * x
-    y_rotated = y
-    return x_rotated, y_rotated, z_rotated
-
 def translate_3D_coordinates_along_axes(x, y, z, shift_x, shift_y, shift_z):
     '''Translates coordinates along one or more axes'''
     x_shifted = x + shift_x
     y_shifted = y + shift_y
     z_shifted = z + shift_z
     return x_shifted, y_shifted, z_shifted
+
 
 def scale_3D_coordinates(x, y, z, x_scale_factor, y_scale_factor=None, z_scale_factor=None):
     '''Scales the given coordinates by the given scale factor.'''
@@ -78,29 +76,30 @@ def mirror_3D_coordinates_at_axis(x, y, z, axis, mirror_at_axis_coordinate=None)
         raise ValueError("ERROR: axis must be one of {'x', 'y', 'z'}")
 
     if axis == 'x':
-        return mirror_coordinates_at_axis(x, mirror_at_axis_coordinate), np.copy(y), np.copy(z)
+        return _mirror_coordinates_at_axis(x, mirror_at_axis_coordinate), np.copy(y), np.copy(z)
     elif axis == 'y':
-        return np.copy(x), mirror_coordinates_at_axis(y, mirror_at_axis_coordinate), np.copy(z)
+        return np.copy(x), _mirror_coordinates_at_axis(y, mirror_at_axis_coordinate), np.copy(z)
     else:
-        return np.copy(x), np.copy(y), mirror_coordinates_at_axis(z, mirror_at_axis_coordinate)
+        return np.copy(x), np.copy(y), _mirror_coordinates_at_axis(z, mirror_at_axis_coordinate)
 
-def mirror_coordinates_at_axis(c, mirror_at_axis_coordinate=None):
+def _mirror_coordinates_at_axis(c, mirror_at_axis_coordinate=None):
     '''Mirrors or reflects a 1-dimensional array of coordinates on a plane (perpendicular to the axis) at the given axis coordinate. If no coordinate is given, the minimum value of the coordinates is used.'''
     if mirror_at_axis_coordinate is None:
         mirror_at_axis_coordinate = np.min(c)
     c_mirrored = mirror_at_axis_coordinate - (c - mirror_at_axis_coordinate)
     return c_mirrored
 
+
 def point_mirror_3D_coordinates(x, y, z, point_x, point_y, point_z):
     '''Point-mirrors or reflects the given coordinates at the given point.'''
-    return mirror_coordinates_at_axis(x, point_x), mirror_coordinates_at_axis(y, point_y), mirror_coordinates_at_axis(z, point_z)
+    return _mirror_coordinates_at_axis(x, point_x), _mirror_coordinates_at_axis(y, point_y), _mirror_coordinates_at_axis(z, point_z)
 
 
 def rad2deg(rad):
     """
     Convert an angle given in radians to degrees.
 
-    Convert an angle given in radians to degrees. Pi radians are 360 degrees.
+    Convert an angle given in radians to degrees. 2 Pi radians are 360 degrees. If negative values or values larger than 2 Pi are passed, use the modulo operation to bring them to a suitable range first. In other words, passing -0.5 * Pi will be transformed to 2 - 0.5 = 1.5 Pi, and will thus return 270 degrees.
 
     Parameters
     ----------
@@ -117,17 +116,24 @@ def rad2deg(rad):
         rad = adjusted
     return rad * 180 / np.pi
 
-def deg2rad(degree):
-    '''
-    Convert an angle given in degree to radians.
-    '''
-    if degree < 0 or degree > 360:
-        adjusted = degree % 360
-        degree = adjusted
-    return degree * np.pi / 180
 
-def rotate_3D_coordinates_around_z_axis(x, y, z, rot):
-    x_rotated = np.cos(rot) * x - np.sin(rot) * y
-    y_rotated = np.sin(rot) * x + np.cos(rot) * y
-    z_rotated = z
-    return x_rotated, y_rotated, z_rotated
+def deg2rad(degrees):
+    """
+    Convert an angle given in degrees to radians.
+
+    Convert an angle given in degrees to radians. 360 degrees are 2 Pi radians. If negative values or values larger than 360 are passed, use the modulo operation to bring them to a suitable range first. In other words, passing -90 will be transformed to 360 - 90 = 270 degrees, and will thus return 1.5 Pi radians.
+
+    Parameters
+    ----------
+    degrees : float
+        The angle in degrees.
+
+    Returns
+    -------
+    float
+        The angle in radians.
+    """
+    if degrees < 0 or degrees > 360:
+        adjusted = degrees % 360
+        degrees = adjusted
+    return degrees * np.pi / 180
--- tests/cogload/test_spatial_transform.py
+++ tests/cogload/test_spatial_transform.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_equal, assert_allclose
 import cogload as cl
 import cogload.spatial_transform as st
 
@@ -8,13 +8,21 @@ def test_rad2deg_with_positive_input():
     deg = st.rad2deg(2 * np.pi)
     assert deg == pytest.approx(360.0, 0.01)
 
+
+def test_rad2deg_with_negative_input():
+    deg = st.rad2deg(- 0.5 * np.pi)
+    assert deg == pytest.approx(270.0, 0.01)
+
+
 def test_deg2rad_with_positive_input():
     rad = st.deg2rad(180)
     assert rad == pytest.approx(np.pi, 0.01)
 
+
 def test_deg2rad_with_negative_input():
     rad = st.deg2rad(-90)
     assert rad == pytest.approx(np.pi * 1.5, 0.01)
+
 
 def test_coords_a2s_single_values():
     coords = np.array([[5, 7, 9]])
@@ -22,6 +30,7 @@ def test_coords_a2s_single_values():
     assert x == 5
     assert y == 7
     assert z == 9
+
 
 def test_coords_a2s_arrays():
     coords = np.array([[5, 7, 9], [6, 8, 10]])
@@ -43,6 +52,7 @@ def test_coords_s2a_single_values():
     assert coords[0][1] == 7
     assert coords[0][2] == 9
 
+
 def test_coords_s2a_arrays():
     x = np.array([5, 6])
     y = np.array([7, 8])
@@ -55,6 +65,7 @@ def test_coords_s2a_arrays():
     assert coords[1][1] == 8
     assert coords[1][2] == 10
 
+
 def test_applying_s2a_followed_by_a2s_is_identical_to_start_value():
     coords = np.array([[5, 7, 9], [6, 8, 10]])
     x, y, z = st.coords_a2s(coords)
@@ -62,6 +73,147 @@ def test_applying_s2a_followed_by_a2s_is_identical_to_start_value():
     assert_array_equal(coords, coords_new)
 
 
-def test_rotate_3D_coordinates_around_axis_with_x_axis():
-    coords = np.array([])
-    #rotate_3D_coordinates_around_axis()
+def test_rotate_3D_coordinates_around_axes_with_x_axis():
+    x = np.array([5, 6])
+    y = np.array([7, 8])
+    z = np.array([9, 10])
+    xr, yr, zr = st.rotate_3D_coordinates_around_axes(x, y, z, np.pi, 0, 0)
+    expected_xr = np.array([5, 6])
+    expected_yr = np.array([-7, -8])
+    expected_zr = np.array([-9, -10])
+    assert_allclose(xr, expected_xr)
+    assert_allclose(yr, expected_yr)
+    assert_allclose(zr, expected_zr)
+
+
+def test_rotate_3D_coordinates_around_axes_with_y_axis():
+    x = np.array([5, 6])
+    y = np.array([7, 8])
+    z = np.array([9, 10])
+    xr, yr, zr = st.rotate_3D_coordinates_around_axes(x, y, z, 0, np.pi, 0)
+    expected_xr = np.array([-5, -6])
+    expected_yr = np.array([7, 8])
+    expected_zr = np.array([-9, -10])
+    assert_allclose(xr, expected_xr)
+    assert_allclose(yr, expected_yr)
+    assert_allclose(zr, expected_zr)
+
+def test_rotate_3D_coordinates_around_axes_with_z_axis():
+    x = np.array([5, 6])
+    y = np.array([7, 8])
+    z = np.array([9, 10])
+    xr, yr, zr = st.rotate_3D_coordinates_around_axes(x, y, z, 0, 0, np.pi)
+    expected_xr = np.array([-5, -6])
+    expected_yr = np.array([-7, -8])
+    expected_zr = np.array([9, 10])
+    assert_allclose(xr, expected_xr)
+    assert_allclose(yr, expected_yr)
+    assert_allclose(zr, expected_zr)
+
+def test_mirror_3D_coordinates_at_axis_with_x_axis_no_exlicit_value():
+    x = np.array([5, 6])
+    y = np.array([7, 8])
+    z = np.array([9, 10])
+    xm, ym, zm = st.mirror_3D_coordinates_at_axis(x, y, z, 'x')
+    expected_xm = np.array([5, 4])
+    expected_ym = np.array([7, 8])
+    expected_zm = np.array([9, 10])
+    assert_allclose(xm, expected_xm)
+    assert_allclose(ym, expected_ym)
+    assert_allclose(zm, expected_zm)
+
+def test_mirror_3D_coordinates_at_axis_with_x_axis_with_exlicit_value():
+    x = np.array([5, 6])
+    y = np.array([7, 8])
+    z = np.array([9, 10])
+    xm, ym, zm = st.mirror_3D_coordinates_at_axis(x, y, z, 'x', 0)
+    expected_xm = np.array([-5, -6])
+    expected_ym = np.array([7, 8])
+    expected_zm = np.array([9, 10])
+    assert_allclose(xm, expected_xm)
+    assert_allclose(ym, expected_ym)
+    assert_allclose(zm, expected_zm)
+
+def test_mirror_3D_coordinates_at_axis_with_y_axis_no_exlicit_value():
+    x = np.array([5, 6])
+    y = np.array([7, 8])
+    z = np.array([9, 10])
+    xm, ym, zm = st.mirror_3D_coordinates_at_axis(x, y, z, 'y')
+    expected_xm = np.array([5, 6])
+    expected_ym = np.array([7, 6])
+    expected_zm = np.array([9, 10])
+    assert_allclose(xm, expected_xm)
+    assert_allclose(ym, expected_ym)
+    assert_allclose(zm, expected_zm)
+
+def test_mirror_3D_coordinates_at_axis_with_y_axis_with_exlicit_value():
+    x = np.array([5, 6])
+    y = np.array([7, 8])
+    z = np.array([9, 10])
+    xm, ym, zm = st.mirror_3D_coordinates_at_axis(x, y, z, 'y', 0)
+    expected_xm = np.array([5, 6])
+    expected_ym = np.array([-7, -8])
+    expected_zm = np.array([9, 10])
+    assert_allclose(xm, expected_xm)
+    assert_allclose(ym, expected_ym)
+    assert_allclose(zm, expected_zm)
+
+def test_mirror_3D_coordinates_at_axis_with_z_axis_no_exlicit_value():
+    x = np.array([5, 6])
+    y = np.array([7, 8])
+    z = np.array([9, 10])
+    xm, ym, zm = st.mirror_3D_coordinates_at_axis(x, y, z, 'z')
+    expected_xm = np.array([5, 6])
+    expected_ym = np.array([7, 8])
+    expected_zm = np.array([9, 8])
+    assert_allclose(xm, expected_xm)
+    assert_allclose(ym, expected_ym)
+    assert_allclose(zm, expected_zm)
+
+def test_mirror_3D_coordinates_at_axis_with_z_axis_with_exlicit_value():
+    x = np.array([5, 6])
+    y = np.array([7, 8])
+    z = np.array([9, 10])
+    xm, ym, zm = st.mirror_3D_coordinates_at_axis(x, y, z, 'z', -2)
+    expected_xm = np.array([5, 6])
+    expected_ym = np.array([7, 8])
+    expected_zm = np.array([-13, -14])
+    assert_allclose(xm, expected_xm)
+    assert_allclose(ym, expected_ym)
+    assert_allclose(zm, expected_zm)
+
+def test_point_mirror_3D_coordinates():
+    x = np.array([5, 6])
+    y = np.array([7, 8])
+    z = np.array([9, 10])
+    xm, ym, zm = st.point_mirror_3D_coordinates(x, y, z, 0, 0, 0)
+    expected_xm = np.array([-5, -6])
+    expected_ym = np.array([-7, -8])
+    expected_zm = np.array([-9, -10])
+    assert_allclose(xm, expected_xm)
+    assert_allclose(ym, expected_ym)
+    assert_allclose(zm, expected_zm)
+
+def test_translate_3D_coordinates_along_axes():
+    x = np.array([5, 6])
+    y = np.array([7, 8])
+    z = np.array([9, 10])
+    xt, yt, zt = st.translate_3D_coordinates_along_axes(x, y, z, 2, -4, 0)
+    expected_xt = np.array([7, 8])
+    expected_yt = np.array([3, 4])
+    expected_zt = np.array([9, 10])
+    assert_allclose(xt, expected_xt)
+    assert_allclose(yt, expected_yt)
+    assert_allclose(zt, expected_zt)
+
+def test_scale_3D_coordinates():
+    x = np.array([5, 6])
+    y = np.array([7, 8])
+    z = np.array([9, 10])
+    xs, ys, zs = st.scale_3D_coordinates(x, y, z, 3.0)
+    expected_xs = np.array([15, 18])
+    expected_ys = np.array([21, 24])
+    expected_zs = np.array([27, 30])
+    assert_allclose(xs, expected_xs)
+    assert_allclose(ys, expected_ys)
+    assert_allclose(zs, expected_zs)
