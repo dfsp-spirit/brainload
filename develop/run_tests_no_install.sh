74ff09a9ea72f2242f64662617bff0a5e3569dd6
Tim Schaefer <ts+code@rcmd.org>
2018-10-09 11:11:41 +0200

    check for correct path in script

--- develop/run_tests_no_install.sh
+++ develop/run_tests_no_install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-## run-tests.sh -- Runs the unit tests for cogload.
-## You should execute this script from with its directory, i.e., as `./run_tests.sh`.
+## run-tests.sh -- Runs the unit tests for cogload without installation, i.e., from the source in the local dir.
+## You should execute this script from the repo root dir, i.e., as `./develop/run_tests.sh`.
+
+# Check whether we are in correct dir
+if [ ! -d "src/cogload" ]; then
+    echo "ERROR: Run this script from the repo root."
+    exit 1
+fi
+
 PYTHONPATH=src pytest --pyargs tests/
