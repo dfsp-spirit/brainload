a5efb09c2c8385f3e2c9d93330b673f4c81eaa1f
Tim Schaefer <ts+code@rcmd.org>
2018-10-10 18:08:31 +0200

    fix bug: remove quotes

--- develop/get_test_data_fsaverage.bash
+++ develop/get_test_data_fsaverage.bash
@@ -14,7 +14,7 @@ if [ -n "${FREESURFER_HOME}" ]; then
     FD="${FREESURFER_HOME}/subjects/fsaverage/surf"
     mkdir -p "${COGLOAD_TEST_DATA_DIR_FSAVERAGE_SURF}"
     echo "Copying fsaverage data from '${FD}' to '${COGLOAD_TEST_DATA_DIR_FSAVERAGE_SURF}'."
-    for TFILE in "lh.inflated lh.pial lh.sphere lh.white rh.inflated rh.pial rh.sphere rh.white"; do
+    for TFILE in lh.inflated lh.pial lh.sphere lh.white rh.inflated rh.pial rh.sphere rh.white; do
         cp "${FD}/${TFILE}" "${COGLOAD_TEST_DATA_DIR_FSAVERAGE_SURF}" && echo " * ${TFILE}"
     done
 else
