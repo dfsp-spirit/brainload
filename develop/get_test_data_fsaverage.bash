446e3d636a23fd17ce3e1d82ea5605fd4b4e044e
Tim Schaefer <ts+code@rcmd.org>
2018-10-11 11:06:47 +0200

    also copy FreeSurfer license file when copying FreeSurfer fsaverage data.

--- develop/get_test_data_fsaverage.bash
+++ develop/get_test_data_fsaverage.bash
@@ -2,7 +2,8 @@
 # get_test_data_fsaverage.bash
 
 COGLOAD_BASE_DIR="."
-COGLOAD_TEST_DATA_DIR_FSAVERAGE_SURF="${COGLOAD_BASE_DIR}/tests/test_data/fsaverage/surf"
+COGLOAD_TEST_DATA_DIR_FSAVERAGE="${COGLOAD_BASE_DIR}/tests/test_data/fsaverage"
+COGLOAD_TEST_DATA_DIR_FSAVERAGE_SURF="${COGLOAD_TEST_DATA_DIR_FSAVERAGE}/surf"
 
 # Check whether we are in correct dir
 if [ ! -d "src/cogload" ]; then
@@ -13,6 +14,12 @@ fi
 if [ -n "${FREESURFER_HOME}" ]; then
     FD="${FREESURFER_HOME}/subjects/fsaverage/surf"
     mkdir -p "${COGLOAD_TEST_DATA_DIR_FSAVERAGE_SURF}"
+
+    # Copy the FreeSurfer license, as the fsaverage data we copy is part of FreeSurfer
+    echo "Copying FreeSurfer license file from '${FREESURFER_HOME}' to '${COGLOAD_TEST_DATA_DIR_FSAVERAGE}'."
+    cp "${FREESURFER_HOME}/LICENSE" "${COGLOAD_TEST_DATA_DIR_FSAVERAGE}"
+
+    # Copy only the data we need for testing. The whole fsaverage subject is vast.
     echo "Copying fsaverage data from '${FD}' to '${COGLOAD_TEST_DATA_DIR_FSAVERAGE_SURF}'."
     for TFILE in lh.inflated lh.pial lh.sphere lh.white rh.inflated rh.pial rh.sphere rh.white; do
         cp "${FD}/${TFILE}" "${COGLOAD_TEST_DATA_DIR_FSAVERAGE_SURF}" && echo " * ${TFILE}"
