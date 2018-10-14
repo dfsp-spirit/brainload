2fc35b69f91f5adff3f5f005fef8f10097a9b389
Tim Schäfer <ts+code@rcmd.org>
2018-10-14 14:01:18 +0200

    fix references to old project name in script

--- develop/example_client/run_example_no_install.sh
+++ develop/example_client/run_example_no_install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-## This shell script runs a demo client which uses cogload without installation, directly from the source directory.
+## This shell script runs a demo client which uses brainload without installation, directly from the source directory.
 ## It is for internal development only and will not work on other machines as it loads data that only exists on my machine. I guess you could supply your own MRI/FreeSurfer data and use it, but it does nothing to fancy.
 STUDY_DATA_DIR="${HOME}/data/tim_only"
-COGLOAD_DIR="${HOME}/develop/cogload"
-COGLOAD_SOURCE_DIR="${COGLOAD_DIR}/src"
+BRAINLOAD_DIR="${HOME}/develop/brainload"
+BRAINLOAD_SOURCE_DIR="${BRAINLOAD_DIR}/src"
 
 export SUBJECTS_DIR="${STUDY_DATA_DIR}"
-cd "${STUDY_DATA_DIR}" && PYTHONPATH=${PYTHONPATH}:${COGLOAD_SOURCE_DIR} python "${COGLOAD_DIR}/develop/example_client/example.py"
+cd "${STUDY_DATA_DIR}" && PYTHONPATH=${PYTHONPATH}:${BRAINLOAD_SOURCE_DIR} python "${BRAINLOAD_DIR}/develop/example_client/example.py"
