b19a95c551597159a893ed13e2530f1ade30c036
Tim Schaefer <ts+code@rcmd.org>
2018-10-14 21:46:34 +0200

    update scripts: rename, add APPTAG to all output

--- develop/get_test_data_fsaverage.bash
+++ develop/get_test_data_fsaverage.bash
@@ -8,20 +8,23 @@
 ## See https://github.com/freesurfer/freesurfer/blob/dev/LICENSE or your local copy at $FREESURFER_HOME/LICENSE if you have FreeSurfer installed.
 ## The license will also be available in the generated data directory when the script has finished.
 
+APPTAG="[TD_FSAVG]"
+
 if [ -z "${BRAINLOAD_TEST_DATA_DIR}" ]; then
     # Check whether we are in correct dir
     if [ ! -d "tests/test_data" ]; then
-        echo "ERROR: Run this script from the repo root or set the environment variable BRAINLOAD_TEST_DATA_DIR."
+        echo "${APPTAG} ERROR: Run this script from the repo root or set the environment variable BRAINLOAD_TEST_DATA_DIR."
         exit 1
     else
         BRAINLOAD_TEST_DATA_DIR="tests/test_data"        # This is the level equivalent to SUBJECTS_DIR from FreeSurfer
+        echo "${APPTAG} Environment variable BRAINLOAD_TEST_DATA_DIR was not set, assuming '${BRAINLOAD_TEST_DATA_DIR}'."
     fi
 else
-    echo "INFO: Environment variable BRAINLOAD_TEST_DATA_DIR is set, using test data dir '${BRAINLOAD_TEST_DATA_DIR}'."
+    echo "${APPTAG} INFO: Environment variable BRAINLOAD_TEST_DATA_DIR is set, using test data dir '${BRAINLOAD_TEST_DATA_DIR}'."
 fi
 
 if [ ! -d "${BRAINLOAD_TEST_DATA_DIR}" ]; then
-    echo "ERROR: The test data directory '${BRAINLOAD_TEST_DATA_DIR}' does not exist. Please fix the environment variable BRAINLOAD_TEST_DATA_DIR."
+    echo "${APPTAG} ERROR: The test data directory '${BRAINLOAD_TEST_DATA_DIR}' does not exist. Please fix the environment variable BRAINLOAD_TEST_DATA_DIR."
     exit 1
 fi
 
@@ -48,42 +51,42 @@ mkdir -p "${BRAINLOAD_TEST_DATA_DIR_FSAVERAGE_SURF}" || { echo "ERROR: Could not
 
 if [ "${MODE}" = "local_then_remote" -o "${MODE}" = "local" ]; then
 
-    echo "Trying to get data from local FreeSurfer installation..."
+    echo "${APPTAG} Trying to get data from local FreeSurfer installation..."
 
     if [ -n "${FREESURFER_HOME}" ]; then
         FD="${FREESURFER_HOME}/subjects/fsaverage/surf"
 
         # Copy the FreeSurfer license (and potentially other meta data files), as the fsaverage data we copy is part of FreeSurfer
-        echo "Copying FreeSurfer license file from '${FREESURFER_HOME}' to '${BRAINLOAD_TEST_DATA_DIR_FSAVERAGE}'."
+        echo "${APPTAG} Copying FreeSurfer license file from '${FREESURFER_HOME}' to '${BRAINLOAD_TEST_DATA_DIR_FSAVERAGE}'."
         for LFILE in LICENSE; do
-            cp "${FREESURFER_HOME}/${LFILE}" "${BRAINLOAD_TEST_DATA_DIR_FSAVERAGE_SURF}" && echo " * ${LFILE}"
+            cp "${FREESURFER_HOME}/${LFILE}" "${BRAINLOAD_TEST_DATA_DIR_FSAVERAGE_SURF}" && echo "${APPTAG}  * ${LFILE}"
         done
 
         # Copy only the data we need for testing. The whole fsaverage subject is vast.
-        echo "Copying fsaverage data from '${FD}' to '${BRAINLOAD_TEST_DATA_DIR_FSAVERAGE_SURF}'."
+        echo "${APPTAG} Copying fsaverage data from '${FD}' to '${BRAINLOAD_TEST_DATA_DIR_FSAVERAGE_SURF}'."
         for TFILE in lh.inflated lh.pial lh.sphere lh.white rh.inflated rh.pial rh.sphere rh.white; do
-            cp "${FD}/${TFILE}" "${BRAINLOAD_TEST_DATA_DIR_FSAVERAGE_SURF}" && echo " * ${TFILE}"
+            cp "${FD}/${TFILE}" "${BRAINLOAD_TEST_DATA_DIR_FSAVERAGE_SURF}" && echo "${APPTAG}  * ${TFILE}"
         done
 
         DONE_ALREADY="YES" # We should do error checking, really.
 
     else
         if [ "${MODE}" = "local" ]; then
-            echo "ERROR: Local-only mode selected, but environment variable FREESURFER_HOME not set. Please install and configure FreeSurfer."
+            echo "${APPTAG} ERROR: Local-only mode selected, but environment variable FREESURFER_HOME not set. Please install and configure FreeSurfer."
         else
-            echo "No local FreeSurfer installation found (Hint: set FREESURFER_HOME properly in case you have one)."
+            echo "${APPTAG} No local FreeSurfer installation found (Hint: set FREESURFER_HOME properly in case you have one)."
         fi
     fi
 fi
 
 if [ "${MODE}" = "local_then_remote" -a ${DONE_ALREADY} = "NO" -o "${MODE}" = "remote" ]; then
 
-    echo "Trying to download data from remote location '${REMOTE_ZIP_URL}'..."
+    echo "${APPTAG} Trying to download data from remote location '${REMOTE_ZIP_URL}'..."
 
 
     if [ -f "${BRAINLOAD_TEST_DATA_DIR}/${ARCHIVE_NAME}" ]; then
         rm "${BRAINLOAD_TEST_DATA_DIR}/${ARCHIVE_NAME}"
     fi
 
-    cd "${BRAINLOAD_TEST_DATA_DIR}" && wget "${REMOTE_ZIP_URL}" && unzip -f -o ${ARCHIVE_NAME} && rm ${ARCHIVE_NAME} && echo "OK."
+    cd "${BRAINLOAD_TEST_DATA_DIR}" && wget "${REMOTE_ZIP_URL}" && unzip -f -o ${ARCHIVE_NAME} && rm ${ARCHIVE_NAME} && echo "${APPTAG} OK."
 fi
--- develop/get_test_data_group.bash
+++ develop/get_test_data_group.bash
@@ -1,43 +1,45 @@
 #!/bin/bash
-## get_group_data.bash -- Generates the subjects subject2, subject3, ..., subject5 by copying subject1. Also writes a subjects.txt file for these fake subjects.
+## get_test_data_group.bash -- Generates the subjects subject2, subject3, ..., subject5 by copying subject1. Also writes a subjects.txt file for these fake subjects.
 ## author: Tim Schäfer
 ## This file is part of brainload. Copright Tim Schäfer, 2018. See the LICENSE file for the license.
 
+APPTAG="[TD_GRP]"
 
 if [ -z "${BRAINLOAD_TEST_DATA_DIR}" ]; then
     # Check whether we are in correct dir
     if [ ! -d "tests/test_data" ]; then
-        echo "ERROR: Run this script from the repo root or set the environment variable BRAINLOAD_TEST_DATA_DIR."
+        echo "${APPTAG} ERROR: Run this script from the repo root or set the environment variable BRAINLOAD_TEST_DATA_DIR."
         exit 1
     else
         BRAINLOAD_TEST_DATA_DIR="tests/test_data"        # This is the level equivalent to SUBJECTS_DIR from FreeSurfer
+        echo "${APPTAG} Environment variable BRAINLOAD_TEST_DATA_DIR was not set, assuming '${BRAINLOAD_TEST_DATA_DIR}'."
     fi
 else
-    echo "INFO: Environment variable BRAINLOAD_TEST_DATA_DIR is set, using test data dir '${BRAINLOAD_TEST_DATA_DIR}'."
+    echo "${APPTAG} INFO: Environment variable BRAINLOAD_TEST_DATA_DIR is set, using test data dir '${BRAINLOAD_TEST_DATA_DIR}'."
 fi
 
 if [ ! -d "${BRAINLOAD_TEST_DATA_DIR}" ]; then
-    echo "ERROR: The test data directory '${BRAINLOAD_TEST_DATA_DIR}' does not exist. Please fix the environment variable BRAINLOAD_TEST_DATA_DIR."
+    echo "${APPTAG} ERROR: The test data directory '${BRAINLOAD_TEST_DATA_DIR}' does not exist. Please fix the environment variable BRAINLOAD_TEST_DATA_DIR."
     exit 1
 fi
 
 
 if [ -d "${BRAINLOAD_TEST_DATA_DIR}/subject1" ]; then
 
-    cd "${BRAINLOAD_TEST_DATA_DIR}" || { echo "ERROR: Could not change into directory '${BRAINLOAD_TEST_DATA_DIR}'." ; exit 1; }
+    cd "${BRAINLOAD_TEST_DATA_DIR}" || { echo "${APPTAG} ERROR: Could not change into directory '${BRAINLOAD_TEST_DATA_DIR}'." ; exit 1; }
 
     SUBJECTS_FILE='subjects.txt'
-    echo "Creating subjects file '${SUBJECTS_FILE}' in directory '${BRAINLOAD_TEST_DATA_DIR}' and adding subject1 ..."
-    echo "subject1" > "${SUBJECTS_FILE}" || { echo "ERROR: Could not write to subjects file '${SUBJECTS_FILE}'. Check write permissions for file and dir '${BRAINLOAD_TEST_DATA_DIR}'." ; exit 1; }
+    echo "${APPTAG} Creating subjects file '${SUBJECTS_FILE}' in directory '${BRAINLOAD_TEST_DATA_DIR}' and adding subject1 ..."
+    echo "subject1" > "${SUBJECTS_FILE}" || { echo "${APPTAG} ERROR: Could not write to subjects file '${SUBJECTS_FILE}'. Check write permissions for file and dir '${BRAINLOAD_TEST_DATA_DIR}'." ; exit 1; }
 
-    echo "Creating new test subjects in directory '${BRAINLOAD_TEST_DATA_DIR}' ..."
+    echo "${APPTAG} Creating new test subjects in directory '${BRAINLOAD_TEST_DATA_DIR}' ..."
     for SUBJECT_NUMBER in {2..5}; do
         NEW_SUBJECT_ID="subject${SUBJECT_NUMBER}"
-        cp -r subject1 "${NEW_SUBJECT_ID}" && echo "${NEW_SUBJECT_ID}" >> "${SUBJECTS_FILE}" && echo " * ${NEW_SUBJECT_ID}"
+        cp -r subject1 "${NEW_SUBJECT_ID}" && echo "${NEW_SUBJECT_ID}" >> "${SUBJECTS_FILE}" && echo "${APPTAG}  * ${NEW_SUBJECT_ID}"
     done
 else
-    echo "ERROR: Data for subject1 not found at '${BRAINLOAD_TEST_DATA_DIR}/subject1'. Exiting."
+    echo "${APPTAG} ERROR: Data for subject1 not found at '${BRAINLOAD_TEST_DATA_DIR}/subject1'. Exiting."
     exit 1
 fi
 
-echo "Done. Please check the data in '${BRAINLOAD_TEST_DATA_DIR}'."
+echo "${APPTAG} Done. Please check the data in '${BRAINLOAD_TEST_DATA_DIR}'."
