4b19055f8293c13edab0ce67a60871d0ad045baf
Tim Schaefer <ts+code@rcmd.org>
2018-10-16 21:15:29 +0200

    add some more tests

--- tests/brainload/test_nitools.py
+++ tests/brainload/test_nitools.py
@@ -29,17 +29,63 @@ def test_read_subjects_file_one_subject():
     assert 'subject1' in subject_ids
 
 
-def test_detect_subjects_in_directory_with_five_subjects():
+def test_detect_subjects_in_directory_with_six_subjects():
     expected_subject2_dir = os.path.join(TEST_DATA_DIR, 'subject2')
     if not os.path.isdir(expected_subject2_dir):
         pytest.skip("Test data for subject2 .. subject5 not available: e.g., directory '%s' does not exist. You can get it by running the 'get_test_data_all.bash' script." % expected_subject2_dir)
     subject_ids = nit.detect_subjects_in_directory(TEST_DATA_DIR)
-    assert len(subject_ids) == 5
+    assert len(subject_ids) == 6
     assert 'subject1' in subject_ids
     assert 'subject2' in subject_ids
     assert 'subject3' in subject_ids
     assert 'subject4' in subject_ids
     assert 'subject5' in subject_ids
+    assert 'subject6' in subject_ids
+    assert not 'fsaverage' in subject_ids                       # the default setting is to ignore 'fsaverage'
+    assert not 'subject_files_tab_separated' in subject_ids     # this dir exists, but it has no sub dir 'surf' and should thus not be listed
+
+
+def test_detect_subjects_in_directory_does_not_ignore_fsaverage_when_ignore_list_is_empty():
+    expected_subject2_dir = os.path.join(TEST_DATA_DIR, 'subject2')
+    if not os.path.isdir(expected_subject2_dir):
+        pytest.skip("Test data for subject2 .. subject5 not available: e.g., directory '%s' does not exist. You can get it by running the 'get_test_data_all.bash' script." % expected_subject2_dir)
+
+    subject_ids = nit.detect_subjects_in_directory(TEST_DATA_DIR, ignore_dir_names=[])
+    assert len(subject_ids) == 7
+    assert 'subject1' in subject_ids
+    assert 'subject2' in subject_ids
+    assert 'subject3' in subject_ids
+    assert 'subject4' in subject_ids
+    assert 'subject5' in subject_ids
+    assert 'subject6' in subject_ids
+    assert 'fsaverage' in subject_ids
+    assert not 'subject_files_tab_separated' in subject_ids     # this dir exists, but it has no sub dir 'surf' and should thus not be listed
+
+
+def test_detect_subjects_in_directory_does_not_ignores_subjects_when_asked_to_via_ignore_listy():
+    expected_subject2_dir = os.path.join(TEST_DATA_DIR, 'subject2')
+    if not os.path.isdir(expected_subject2_dir):
+        pytest.skip("Test data for subject2 .. subject5 not available: e.g., directory '%s' does not exist. You can get it by running the 'get_test_data_all.bash' script." % expected_subject2_dir)
+
+    subject_ids = nit.detect_subjects_in_directory(TEST_DATA_DIR, ignore_dir_names=['subject5', 'subject3'])
+    assert len(subject_ids) == 5
+    assert 'subject1' in subject_ids
+    assert 'subject2' in subject_ids
+    assert not 'subject3' in subject_ids
+    assert 'subject4' in subject_ids
+    assert not 'subject5' in subject_ids
+    assert 'subject6' in subject_ids
+    assert 'fsaverage' in subject_ids
+    assert not 'subject_files_tab_separated' in subject_ids     # this dir exists, but it has no sub dir 'surf' and should thus not be listed
+
+
+def test_detect_subjects_in_directory_does_not_match_anything_when_adding_weird_required_sub_dirs():
+    expected_subject2_dir = os.path.join(TEST_DATA_DIR, 'subject2')
+    if not os.path.isdir(expected_subject2_dir):
+        pytest.skip("Test data for subject2 .. subject5 not available: e.g., directory '%s' does not exist. You can get it by running the 'get_test_data_all.bash' script." % expected_subject2_dir)
+
+    subject_ids = nit.detect_subjects_in_directory(TEST_DATA_DIR, required_subdirs_for_hits=['not_there'])
+    assert len(subject_ids) == 0
 
 
 def test_detect_subjects_in_directory_without_any_subjects():
