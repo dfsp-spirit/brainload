7c0565387a2408c18c929a8c8664fdcdf62e65b0
Tim Schaefer <ts+code@rcmd.org>
2018-10-20 22:11:36 +0200

    make functions show up in sphinx as pacakge functions.

--- src/brainload/__init__.py
+++ src/brainload/__init__.py
@@ -1,1 +1,8 @@
-from .freesurferdata import parse_subject
+"""
+Brainload high-level API functions.
+"""
+
+# The next line makes the listed functions show up in sphinx documentation directly under the package (they also show up under their real sub module, of course)
+__all__ = ['parse_subject', 'load_group_data']
+
+from .freesurferdata import parse_subject, load_group_data
