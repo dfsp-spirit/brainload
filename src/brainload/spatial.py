b04d6ea41c5ca952087f59ad8384044cb2a287ab
Tim Schaefer <ts+code@rcmd.org>
2018-10-23 13:27:48 +0200

    finish documenting the spatial module

--- src/brainload/spatial.py
+++ src/brainload/spatial.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Functions for spatial tranformation of 3-dimensional coordinates.
+Simple functions for spatial tranformation of 3-dimensional coordinates.
 
-These functions are helpful if you want to rotate, translate, mirror, or scale (brain) meshes.
+These functions are helpful if you want to rotate, translate, mirror, or scale (brain) meshes. In general, you would use them roughly like this:
+
+>>> import brainload as bl
+>>> vert_coords, faces = bl.subject('bert')[0:2]
+>>> x, y, z = bl.spatial.coords_a2s(vert_coords)
+
+Now you have the coordinates of the mesh vertices in the required format and can call any function from this module:
+
+>>> xt, yt, zt = bl.spatial.translate_3D_coordinates_along_axes(x, y, z, 5, 0, 0)   # or some other function
+
+
 """
 
 import numpy as np
 
-def rotate_3D_coordinates_around_axes(x, y, z, rotx, roty, rotz):
+def rotate_3D_coordinates_around_axes(x, y, z, radians_x, radians_y, radians_z):
     """
     Rotate coordinates around the 3 axes.
 
@@ -24,13 +34,13 @@ def rotate_3D_coordinates_around_axes(x, y, z, rotx, roty, rotz):
     z: Numpy array of numbers
         A 1D array, representing z axis coordinates. Must have the same length as the `x` and `y` arrays. (See `coords_a2s` if you have a single 2D array containing all 3.)
 
-    rotx: number
+    radians_x: number
         A single number, representing the rotation in radians around the x axis.
 
-    roty: number
+    radians_y: number
         A single number, representing the rotation in radians around the y axis.
 
-    rotz: number
+    radians_z: number
         A single number, representing the rotation in radians around the z axis.
 
     Returns
@@ -52,38 +62,44 @@ def rotate_3D_coordinates_around_axes(x, y, z, rotx, roty, rotz):
     >>> z = np.array([9, 10])
     >>> xr, yr, zr = st.rotate_3D_coordinates_around_axes(x, y, z, np.pi, 0, 0)
     """
-    xr, yr, zr = _rotate_3D_coordinates_around_x_axis(x, y, z, rotx)
-    xr, yr, zr = _rotate_3D_coordinates_around_y_axis(xr, yr, zr, roty)
-    xr, yr, zr = _rotate_3D_coordinates_around_z_axis(xr, yr, zr, rotz)
+    xr, yr, zr = _rotate_3D_coordinates_around_x_axis(x, y, z, radians_x)
+    xr, yr, zr = _rotate_3D_coordinates_around_y_axis(xr, yr, zr, radians_y)
+    xr, yr, zr = _rotate_3D_coordinates_around_z_axis(xr, yr, zr, radians_z)
     return xr, yr, zr
 
 
-def _rotate_3D_coordinates_around_x_axis(x, y, z, rot):
+def _rotate_3D_coordinates_around_x_axis(x, y, z, radians):
     """
     Rotate coordinates around the x axis. Rotation must be given in radians.
+
+    Rotate coordinates around the x axis. See the documentation for `rotate_3D_coordinates_around_axes` for details.
     """
-    y_rotated  = np.cos(rot) * y - np.sin(rot) * z
-    z_rotated  = np.sin(rot) * y + np.cos(rot) * z
+    y_rotated  = np.cos(radians) * y - np.sin(radians) * z
+    z_rotated  = np.sin(radians) * y + np.cos(radians) * z
     x_rotated  = x
     return x_rotated, y_rotated, z_rotated
 
 
-def _rotate_3D_coordinates_around_y_axis(x, y, z, rot):
+def _rotate_3D_coordinates_around_y_axis(x, y, z, radians):
     """
     Rotate coordinates around the y axis. Rotation must be given in radians.
+
+    Rotate coordinates around the y axis. See the documentation for `rotate_3D_coordinates_around_axes` for details.
     """
-    z_rotated = np.cos(rot) * z - np.sin(rot) * x
-    x_rotated = np.sin(rot) * z + np.cos(rot) * x
+    z_rotated = np.cos(radians) * z - np.sin(radians) * x
+    x_rotated = np.sin(radians) * z + np.cos(radians) * x
     y_rotated = y
     return x_rotated, y_rotated, z_rotated
 
 
-def _rotate_3D_coordinates_around_z_axis(x, y, z, rot):
+def _rotate_3D_coordinates_around_z_axis(x, y, z, radians):
     """
     Rotate coordinates around the z axis. Rotation must be given in radians.
+
+    Rotate coordinates around the z axis. See the documentation for `rotate_3D_coordinates_around_axes` for details.
     """
-    x_rotated = np.cos(rot) * x - np.sin(rot) * y
-    y_rotated = np.sin(rot) * x + np.cos(rot) * y
+    x_rotated = np.cos(radians) * x - np.sin(radians) * y
+    y_rotated = np.sin(radians) * x + np.cos(radians) * y
     z_rotated = z
     return x_rotated, y_rotated, z_rotated
 
@@ -148,7 +164,7 @@ def coords_s2a(x, y, z):
 
     Examples
     --------
-    >>> import brainload.spatial as st; import numpy as np;
+    >>> import brainload.spatial as st; import numpy as np
     >>> x = np.array([5, 6])
     >>> y = np.array([7, 8])
     >>> z = np.array([9, 10])
@@ -172,13 +188,13 @@ def translate_3D_coordinates_along_axes(x, y, z, shift_x, shift_y, shift_z):
     Parameters
     ----------
     x: Numpy array of numbers
-        A 1D array representing x axis coordinates. Must have the same length as the `y` and `z` arrays. (See `coords_a2s` if you have a single 2D array containing all 3.)
+        A 1D array representing x axis coordinates. Must have the same length as the `y` and `z` arrays.
 
     y: Numpy array of numbers
-        A 1D array representing y axis coordinates. Must have the same length as the `x` and `z` arrays. (See `coords_a2s` if you have a single 2D array containing all 3.)
+        A 1D array representing y axis coordinates. Must have the same length as the `x` and `z` arrays.
 
     z: Numpy array of numbers
-        A 1D array, representing z axis coordinates. Must have the same length as the `x` and `y` arrays. (See `coords_a2s` if you have a single 2D array containing all 3.)
+        A 1D array, representing z axis coordinates. Must have the same length as the `x` and `y` arrays.
 
     shift_x: number
         A single number, representing the shift along the x axis.
@@ -202,7 +218,13 @@ def translate_3D_coordinates_along_axes(x, y, z, shift_x, shift_y, shift_z):
 
     Examples
     --------
-    >>> import brainload.spatial as st; import numpy as np;
+    >>> import brainload.spatial as st; import numpy as np
+    >>> x = np.array([5, 6])
+    >>> y = np.array([7, 8])
+    >>> z = np.array([9, 10])
+    >>> xt, yt, zt = st.translate_3D_coordinates_along_axes(x, y, z, 2, -4, 0)
+    >>> print "%d %d %d" % (xt[0], yt[0], zt[0])     # 7 3 9
+    >>> print "%d %d %d" % (xt[1], yt[1], zt[1])     # 8 4 10
     """
     x_shifted = x + shift_x
     y_shifted = y + shift_y
@@ -212,7 +234,50 @@ def translate_3D_coordinates_along_axes(x, y, z, shift_x, shift_y, shift_z):
 
 def scale_3D_coordinates(x, y, z, x_scale_factor, y_scale_factor=None, z_scale_factor=None):
     """
-    Scale the given coordinates by the given scale factor.
+    Scale coordinates by factors.
+
+    Scale the given coordinates by the given scale factor or factors.
+
+    Parameters
+    ----------
+    x: Numpy array of numbers
+        A 1D array representing x axis coordinates. Must have the same length as the `y` and `z` arrays.
+
+    y: Numpy array of numbers
+        A 1D array representing y axis coordinates. Must have the same length as the `x` and `z` arrays.
+
+    z: Numpy array of numbers
+        A 1D array, representing z axis coordinates. Must have the same length as the `x` and `y` arrays.
+
+    x_scale_factor: number
+        A single number, representing the scaling factor along the x axis. If the other values are not given, this counts for all axes.
+
+    y_scale_factor: number | None
+        A single number, representing the scaling factor along the y axis. If this is `None`, the value given for `x_scale_factor` is used.
+
+    z_scale_factor: number | None
+        A single number, representing the scaling factor along the z axis. If this is `None`, the value given for `x_scale_factor` is used.
+
+    Returns
+    -------
+    x_scaled: Numpy array of numbers
+        The scaled x coordinates.
+
+    y_scaled: Numpy array of numbers
+        The scaled y coordinates.
+
+    z_scaled: Numpy array of numbers
+        The scaled z coordinates.
+
+    Examples
+    --------
+    >>> import brainload.spatial as st; import numpy as np
+    >>> x = np.array([5, 6])
+    >>> y = np.array([7, 8])
+    >>> z = np.array([9, 10])
+    >>> xs, ys, zs = st.scale_3D_coordinates(x, y, z, 3.0)
+    >>> print "%d %d %d" % (xs[0], ys[0], zs[0])     # 15 21 27
+    >>> print "%d %d %d" % (xs[1], ys[1], zs[1])     # 18 24 30
     """
     if y_scale_factor is None:
         y_scale_factor = x_scale_factor
@@ -228,6 +293,46 @@ def mirror_3D_coordinates_at_axis(x, y, z, axis, mirror_at_axis_coordinate=None)
     Mirror the given 3D coordinates on the given mirror plane.
 
     Mirror or reflect the given 3D coordinates on a plane (perpendicular to the axis) at axis coordinate `mirror_at_axis_coordinate` at the given axis. If `mirror_at_axis_coordinate` is not given, the smallest coordinate along the mirror axis in the data is used.
+
+    Parameters
+    ----------
+    x: Numpy array of numbers
+        A 1D array representing x axis coordinates. Must have the same length as the `y` and `z` arrays.
+
+    y: Numpy array of numbers
+        A 1D array representing y axis coordinates. Must have the same length as the `x` and `z` arrays.
+
+    z: Numpy array of numbers
+        A 1D array, representing z axis coordinates. Must have the same length as the `x` and `y` arrays.
+
+    axis: string, one of {'x', 'y', 'z'}
+        An axis identifier.
+
+    mirror_at_axis_coordinate: number | None
+        The coordinate along the axis `axis` at which the mirror plane should be created. If you set `axis` to 'x' and specify `5` for this, a yz-plane will be used at x coordinate 5. If not given, it defaults to the minimal axis coordinate for the respective axis in the data.
+
+    Returns
+    -------
+    x_mirrored: Numpy array of numbers
+        The mirrored x coordinates.
+
+    y_mirrored: Numpy array of numbers
+        The mirrored y coordinates.
+
+    z_mirrored: Numpy array of numbers
+        The mirrored z coordinates.
+
+    Examples
+    --------
+    Mirror at the origin of the x axis:
+
+    >>> import brainload.spatial as st; import numpy as np
+    >>> x = np.array([5, 6])
+    >>> y = np.array([7, 8])
+    >>> z = np.array([9, 10])
+    >>> xm, ym, zm = st.mirror_3D_coordinates_at_axis(x, y, z, 'x', 0)
+    >>> print "%d %d %d" % (xm[0], ym[0], zm[0])     # -5 7 9
+    >>> print "%d %d %d" % (xm[1], ym[1], zm[1])     # -6 8 10
     """
     if axis not in ('x', 'y', 'z'):
         raise ValueError("ERROR: axis must be one of {'x', 'y', 'z'}")
@@ -238,6 +343,7 @@ def mirror_3D_coordinates_at_axis(x, y, z, axis, mirror_at_axis_coordinate=None)
         return np.copy(x), _mirror_coordinates_at_axis(y, mirror_at_axis_coordinate), np.copy(z)
     else:
         return np.copy(x), np.copy(y), _mirror_coordinates_at_axis(z, mirror_at_axis_coordinate)
+
 
 def _mirror_coordinates_at_axis(c, mirror_at_axis_coordinate=None):
     """
@@ -254,6 +360,49 @@ def _mirror_coordinates_at_axis(c, mirror_at_axis_coordinate=None):
 def point_mirror_3D_coordinates(x, y, z, point_x, point_y, point_z):
     """
     Point-mirror or reflect the given coordinates at the given point.
+
+    Parameters
+    ----------
+    x: Numpy array of numbers
+        A 1D array representing x axis coordinates. Must have the same length as the `y` and `z` arrays.
+
+    y: Numpy array of numbers
+        A 1D array representing y axis coordinates. Must have the same length as the `x` and `z` arrays.
+
+    z: Numpy array of numbers
+        A 1D array, representing z axis coordinates. Must have the same length as the `x` and `y` arrays.
+
+    point_x: number
+        The x coordinate of the point used for mirroring.
+
+    point_y: number
+        The y coordinate of the point used for mirroring.
+
+    point_z: number
+        The z coordinate of the point used for mirroring.
+
+    Returns
+    -------
+    xm: Numpy array of numbers
+        The mirrored x coordinates.
+
+    ym: Numpy array of numbers
+        The mirrored y coordinates.
+
+    zm: Numpy array of numbers
+        The mirrored z coordinates.
+
+    Examples
+    --------
+    Mirror at the origin:
+
+    >>> import brainload.spatial as st; import numpy as np
+    >>> x = np.array([5, 6])
+    >>> y = np.array([7, 8])
+    >>> z = np.array([9, 10])
+    >>> xm, ym, zm = st.point_mirror_3D_coordinates(x, y, z, 0, 0, 0)
+    >>> print "%d %d %d" % (xm[0], ym[0], zm[0])     # -5 -7 -9
+    >>> print "%d %d %d" % (xm[1], ym[1], zm[1])     # -6 -8 -10
     """
     return _mirror_coordinates_at_axis(x, point_x), _mirror_coordinates_at_axis(y, point_y), _mirror_coordinates_at_axis(z, point_z)
 
