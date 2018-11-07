a114d9bda98203b2306e90e9c76e6493a8a9a056
Tim Schaefer <ts+code@rcmd.org>
2018-11-07 10:47:52 +0100

    add version information to init.py

--- src/brainload/__init__.py
+++ src/brainload/__init__.py
@@ -5,4 +5,6 @@ Brainload high-level API functions.
 # The next line makes the listed functions show up in sphinx documentation directly under the package (they also show up under their real sub module, of course)
 __all__ = [ 'subject', 'subject_avg', 'group', 'fsaverage_mesh', 'rhi', 'rhv' ]
 
+__version__ = '0.3.1dev'
+
 from .freesurferdata import subject, subject_avg, group, fsaverage_mesh, rhi, rhv
