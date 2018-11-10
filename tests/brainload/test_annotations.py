a3b4432c15ebc52b3cf2d24c62c7faac49bad37a
Tim Schaefer <ts+code@rcmd.org>
2018-11-10 14:34:31 +0100

    test for raising exception

--- tests/brainload/test_annotations.py
+++ tests/brainload/test_annotations.py
@@ -21,9 +21,10 @@ SUBJECT1_SURF_RH_WHITE_NUM_VERTICES = 153333        # this number is quite arbit
 SUBJECT1_SURF_RH_WHITE_NUM_FACES = 306662           # this number is quite arbitrary: the number of faces is specific for this subject and surface.
 
 SUBJECT1_SURF_LH_WHITE_APARC_ANNOT_NUM_LABELS = 36
+SUBJECT1_SURF_RH_WHITE_APARC_ANNOT_NUM_LABELS = 36
 
 
-def test_read_annotation_md():
+def test_read_annotation_md_lh():
     annotation_file = os.path.join(TEST_DATA_DIR, 'subject1', 'label', 'lh.aparc.annot')
     labels, ctab, names, meta_data = an.read_annotation_md(annotation_file, 'lh', meta_data=None)
     assert len(meta_data) == 1
@@ -31,4 +32,23 @@ def test_read_annotation_md():
     assert labels.shape == (SUBJECT1_SURF_LH_WHITE_NUM_VERTICES, )
     assert ctab.shape == (SUBJECT1_SURF_LH_WHITE_APARC_ANNOT_NUM_LABELS, 5)
     assert len(names) == SUBJECT1_SURF_LH_WHITE_APARC_ANNOT_NUM_LABELS
-    assert names[0] == "unknown"    # first label is known to be unknown
+    assert names[0] == "unknown"    # The first label is known to be 'unknown'. This also tests whether the object really is a string, i.e., whether the bytes have been coverted to string properly for Python 3. This is the real goal.
+
+
+def test_read_annotation_md_rh():
+    annotation_file = os.path.join(TEST_DATA_DIR, 'subject1', 'label', 'rh.aparc.annot')
+    labels, ctab, names, meta_data = an.read_annotation_md(annotation_file, 'rh', meta_data=None)
+    assert len(meta_data) == 1
+    assert meta_data['rh.annotation_file'] == annotation_file
+    assert labels.shape == (SUBJECT1_SURF_RH_WHITE_NUM_VERTICES, )
+    assert ctab.shape == (SUBJECT1_SURF_RH_WHITE_APARC_ANNOT_NUM_LABELS, 5)
+    assert len(names) == SUBJECT1_SURF_RH_WHITE_APARC_ANNOT_NUM_LABELS
+    assert names[0] == "unknown"    # The first label is known to be 'unknown'. This also tests whether the object really is a string, i.e., whether the bytes have been coverted to string properly for Python 3. This is the real goal.
+
+
+def test_read_annotation_md_raises_on_invalid_hemisphere_label():
+    annotation_file = os.path.join(TEST_DATA_DIR, 'subject1', 'label', 'rh.aparc.annot')
+    with pytest.raises(ValueError) as exc_info:
+        labels, ctab, names, meta_data = an.read_annotation_md(annotation_file, 'invalid_hemisphere_label', meta_data=None)
+    assert 'hemisphere_label must be one of' in str(exc_info.value)
+    assert 'invalid_hemisphere_label' in str(exc_info.value)
