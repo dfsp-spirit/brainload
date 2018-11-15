639e8e236c763350ac4f5fdfdcd427072dcbd276
Tim Schaefer <ts+code@rcmd.org>
2018-11-15 11:54:42 +0100

    Remove outdated files

--- develop/example_client/run_example_no_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-## This shell script runs a demo client which uses brainload without installation, directly from the source directory.
-## It is for internal development only and will not work on other machines as it loads data that only exists on my machine. I guess you could supply your own MRI/FreeSurfer data and use it, but it does nothing to fancy.
-STUDY_DATA_DIR="${HOME}/data/tim_only"
-BRAINLOAD_DIR="${HOME}/develop/brainload"
-BRAINLOAD_SOURCE_DIR="${BRAINLOAD_DIR}/src"
-
-export SUBJECTS_DIR="${STUDY_DATA_DIR}"
-cd "${STUDY_DATA_DIR}" && PYTHONPATH=${PYTHONPATH}:${BRAINLOAD_SOURCE_DIR} python "${BRAINLOAD_DIR}/develop/example_client/example.py"
