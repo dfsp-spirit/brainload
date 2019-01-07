eb5baa910eb91368054e2a8951e689fdc2352404
Tim Schaefer <ts+code@rcmd.org>
2019-01-07 12:39:59 +0100

    improve doc strings

--- src/brainload/brainlocate.py
+++ src/brainload/brainlocate.py
@@ -25,14 +25,34 @@ class BrainLocate:
         """
         Find the vertex closest to each of the given coordinates. Requires scipy.
 
+        Parameters
+        ----------
+        query_coords: numpy 2D array
+            The coordinates, given as a numeric array with shape (n, 3) for n coords.
+
         Returns
         -------
-        The index of the closest vertex.
+        numpy 1D array
+            Array with shape (n, ). Each value represents the index of the vertex closest to the respective query coordinate.
         """
         from scipy.spatial.distance import cdist
         return np.argmin(cdist(self.vert_coords, query_coords), axis=0)
 
+
     def get_closest_vertex_and_distance(self, query_coords):
+        """
+        Find the vertex closest to each of the given coordinates, and the respective distance. Requires scipy.
+
+        Parameters
+        ----------
+        query_coords: numpy 2D array
+            The coordinates, given as a numeric array with shape (n, 3) for n coords.
+
+        Returns
+        -------
+        numpy 1D array
+            Array with shape (n, 2). Each row represents the index of the vertex closest to the respective query coordinate (at index 0) and the respective distance (at index 1).
+        """
         from scipy.spatial.distance import cdist
         dist_matrix = cdist(self.vert_coords, query_coords)
         vert_indices = np.argmin(dist_matrix, axis=0)
