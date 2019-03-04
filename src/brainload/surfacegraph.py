5edfd0793db8b50f2d9dc9c237570f00315c41ad
Tim Schaefer <ts+code@rcmd.org>
2019-03-04 21:09:07 +0100

    Add docstrings to surfacegraph functions.

--- src/brainload/surfacegraph.py
+++ src/brainload/surfacegraph.py
@@ -10,10 +10,23 @@ import networkx as nx
 
 class SurfaceGraph:
     """
-    A graph representing a brain surface mesh.
+    A graph representing the vertices and edges of a brain surface mesh.
     """
 
     def __init__(self, verts, faces):
+        """
+        Init the graph from vertices and faces.
+
+        Init the graph from vertices and faces. Note that the faces are not stored directly in the graph, they are turned into edges and the information which edges form a face is lost (this is a graph, not a mesh representation). Also note that the verts and faces parameters which are required here fit the return values of any brainload function which loads a brain mesh, e.g., the ```subject_mesh``` function.
+
+        Parameters
+        ----------
+        verts: numpy 2D float array
+            The vertices in an array with shape (n, 3) for n vertices. The 3 floats are the x, y and z coords of the vertex. The vertex is identified by its index in the array.
+
+        faces: numpy 2D int array
+            The faces in an array with shape (m, 3). Each of the m faces is identified by the indices of the 3 vertices that form it.
+        """
         self.graph = nx.Graph()
         for v_idx, v in enumerate(verts):
             self.graph.add_node(v_idx, x=v[0], y=v[1], z=v[2])
@@ -22,5 +35,22 @@ class SurfaceGraph:
 
 
     def get_neighbors_up_to_dist(self, source_vert, dist):
+        """
+        Get all neighbors up to graph distance dist away.
+
+        Get all neighbors up to graph distance dist away. Note that dist is the number of edges to traverse in the graph to get from the source to the vertex. (It is NOT Euclidian distance.)
+
+        Parameters
+        ----------
+        source_vert: int
+            Index of the source vertex.
+
+        dist: The distance up until which neighbors should be returned. This is the number of edges to traverse in the graph (along a shortest path of length dist from source to the respective nieighbor).
+
+        Returns
+        -------
+        neighbors: set
+            The indices of all vertices which lie within distance dist of the source_vert.
+        """
         dist_dict = nx.single_source_shortest_path_length(self.graph, source_vert, cutoff=dist)
         return dist_dict.keys()
