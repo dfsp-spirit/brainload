e5d43d78393ed0ac26ab9e59fe088d2689a79e92
Tim Schäfer <ts+code@rcmd.org>
2019-05-04 07:09:12 +0200

    add cmd line option to print descriptive stats on morphometry data

--- src/brainload/clients/brain_morph_info.py
+++ src/brainload/clients/brain_morph_info.py
@@ -22,7 +22,9 @@ def brain_morph_info():
     parser.add_argument("curv_file", help="The curv file to load. Must be in Freesurfer curv format. Example files are 'lh.area' or 'rh.thickness'.")
     index_group = parser.add_mutually_exclusive_group(required=True)
     index_group.add_argument("-i", "--index", help="The index of the vertex to query. A single integer or several integers separated by commata (no spaces allowed).")
-    index_group.add_argument("-f", "--index-file", help="A file containing a list of vertex indices.")
+    index_group.add_argument("-f", "--index-file", help="A file containing the list of vertex indices to query.")
+    index_group.add_argument("-a", "--all", help="Select all vertices.", action="store_true")
+    parser.add_argument("-q", "--query", help="The query action to perform. Defaults to 'values'.", default="values", choices=['values', 'describe'])
     parser.add_argument("-s", "--separator", help="Output separator (between vertex coords / indices). Defaults to ','.", default=",")
     parser.add_argument("-v", "--verbose", help="Increase output verbosity.", action="store_true")
     args = parser.parse_args()
@@ -34,25 +36,36 @@ def brain_morph_info():
     if verbose:
         print("---Brain Surface Morphometry Info---")
 
+    hemisphere_label, is_default = fsd._deduce_hemisphere_label_from_file_path(curv_file)
+    morphometry_data, meta_data = fsd.read_fs_morphometry_data_file_and_record_meta_data(curv_file, hemisphere_label)
+
     if args.index:
         query_indices = np.array([int(s) for s in args.index.split(',')], dtype=int)
         if verbose:
             print("Querying curv file for the %d vertex indices from the command line." % (query_indices.shape[0]))
-    else:
+    elif args.index_file:
         query_indices = nit.load_vertex_indices(args.index_file)
         if verbose:
             print("Querying curv file for the %d vertex indices from file '%s'. (File should contain indices separated by '%s'.)" % (query_indices.shape[0], args.index_file, args.separator))
+    else:
+        if verbose:
+            print("Querying curv file for all its %d vertex indices." % (morphometry_data.shape[0]))
+        query_indices = np.arange(morphometry_data.shape[0])
 
-    hemisphere_label, is_default = fsd._deduce_hemisphere_label_from_file_path(curv_file)
-    morphometry_data, meta_data = fsd.read_fs_morphometry_data_file_and_record_meta_data(curv_file, hemisphere_label)
     if verbose:
         print("Morphometry file '%s' contains %d values." % (curv_file, morphometry_data.shape[0]))
 
-    res = sep.join(str(x) for x in morphometry_data[query_indices])
-    if verbose:
-        print("Morphometry values of vertices # %s are: %s" % ([str(x) for x in query_indices], res))
-    else:
-        print(res)
+    if args.query == "values":
+        res = sep.join(str(x) for x in morphometry_data[query_indices])
+        if verbose:
+            print("Morphometry values of vertices # %s are: %s" % ([str(x) for x in query_indices], res))
+        else:
+            print(res)
+    else:   # descriptive stats
+        d = morphometry_data[query_indices]
+        if verbose:
+            print("count, mean, median, .25 quantile, .75 quantile, min, max")
+        print("%d, %f, %f, %f, %f, %f, %f" % (d.shape[0], np.mean(d), np.median(d), np.quantile(d, 0.25), np.quantile(d, 0.75), np.min(d), np.max(d)))
 
     sys.exit(0)
 
