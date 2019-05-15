91a41bbfc0edea36d79dfe84d8eb45d34e49d38e
Tim Schaefer <ts+code@rcmd.org>
2019-05-15 13:34:42 +0200

    Make 2nd test test output

--- tests/clients/test_brain_vol_info.py
+++ tests/clients/test_brain_vol_info.py
@@ -23,5 +23,7 @@ def test_brain_vol_info_by_single_index(script_runner):
     ret = script_runner.run('brain_vol_info', TEST_VOL_FILE, '--crs', '10', '10', '10', '-v')
     assert ret.success
     assert not 'usage' in ret.stdout
+    assert 'Volume has 3 dimensions, shape (256, 256, 256) and data type uint8. It contains 16777216 voxels.' in ret.stdout
+    assert "Received 1 voxel index (with 3 dimensions) from the command line. Printing intensity value of the voxel '10 10 10' in the volume." in ret.stdout
     assert '---Brain Vol Info---' in ret.stdout
     assert ret.stderr == ''
