3923ade2ed85358fd8cdb97db59e74d811cf8f5b
Tim Schäfer <ts+code@rcmd.org>
2019-07-11 21:35:15 +0200

    refactor to remove duplicated code

--- src/brainload/braindescriptors.py
+++ src/brainload/braindescriptors.py
@@ -42,8 +42,30 @@ class BrainDescriptors:
 
         Add brain parcellation atlas stats, e.g., 'aparc' for stats/?h.aparc.annot. (Note that this function is not for segmentation stats like aseg.)
         """
-
         all_subjects_measures_dict, all_subjects_table_data_dict = brainload.stats.group_stats(self.subjects_list, self.subjects_dir, '%s.%s.stats' % (hemi, atlas), stats_table_type_list=brainload.stats.typelist_for_aparc_atlas_stats())
+
+        self._add_measure_dict_stats(all_subjects_measures_dict, atlas)
+        self._add_all_subjects_table_data_stats(all_subjects_table_data_dict, atlas)
+
+
+    def _add_measure_dict_stats(self, all_subjects_measures_dict, atlas):
+        for measure_unique_name in list(all_subjects_measures_dict.keys()):
+            measure_data_all_subjects = all_subjects_measures_dict[measure_unique_name]
+            self.descriptor_values = np.hstack((self.descriptor_values, np.expand_dims(measure_data_all_subjects, axis=1)))
+            self.descriptor_names.append("stats_%s_measure_%s" % (atlas, measure_unique_name))
+
+    def _add_all_subjects_table_data_stats(self, all_subjects_table_data_dict, atlas, ignore_columns=None):
+        if ignore_columns is None:
+            ignore_columns = []
+        region_colum_name = brainload.stats.stats_table_region_label_column_name()
+        region_names = [rname.decode('utf-8') for rname in all_subjects_table_data_dict[region_colum_name][0]]
+
+        for table_column_name in all_subjects_table_data_dict.keys():
+            if table_column_name not in ignore_columns and table_column_name != region_colum_name:
+                all_subjects_all_region_data = all_subjects_table_data_dict[table_column_name]
+                self.descriptor_values = np.hstack((self.descriptor_values, all_subjects_all_region_data))
+                for region_name in region_names:
+                    self.descriptor_names.append("stats_%s_table_%s_%s" % (atlas, table_column_name, region_name))
 
 
     def add_standard_stats(self):
@@ -77,30 +99,19 @@ class BrainDescriptors:
 
         Add brain parcellation stats. This add the data from the stats/aseg.stats file.
         """
+        self.add_single_segmentation_stats('aseg')
+
+
+    def add_single_segmentation_stats(self, atlas):
+        """
+        Add brain parcellation stats.
+
+        Add brain parcellation stats. This add the data from the stats/aseg.stats file.
+        """
         all_subjects_measures_dict, all_subjects_table_data_dict = brainload.stats.group_stats_aseg(self.subjects_list, self.subjects_dir)
 
-        # handle measures dict
-        for measure_unique_name in list(all_subjects_measures_dict.keys()):
-            measure_data_all_subjects = all_subjects_measures_dict[measure_unique_name]
-            self.descriptor_values = np.hstack((self.descriptor_values, np.expand_dims(measure_data_all_subjects, axis=1)))
-            self.descriptor_names.append("stats_aseg_measure_%s" % (measure_unique_name))
-
-        # handle table data
-        region_colum_name = brainload.stats.stats_table_region_label_column_name()
-        region_names = [rname.decode('utf-8') for rname in all_subjects_table_data_dict[region_colum_name][0]]
-
-        for table_column_name in all_subjects_table_data_dict.keys():
-            if table_column_name not in ('Index', 'SegId', region_colum_name):
-                #print("handling table column '%s'" % (table_column_name))
-                all_subjects_all_region_data = all_subjects_table_data_dict[table_column_name]
-                #print("self.descriptor_values has shape %s" % (str(self.descriptor_values.shape)))
-                #print("add received values with shape %s" % (str(all_subjects_all_region_data.shape)))
-                self.descriptor_values = np.hstack((self.descriptor_values, all_subjects_all_region_data))
-                for region_name in region_names:
-                    self.descriptor_names.append("stats_aseg_table_%s_%s" % (table_column_name, region_name))
-
-
-
+        self._add_measure_dict_stats(all_subjects_measures_dict, atlas)
+        self._add_all_subjects_table_data_stats(all_subjects_table_data_dict, atlas, ignore_columns=['Index', 'SegId'])
 
 
     def add_custom_measure_stats(self, atlas_list, measure_list):
